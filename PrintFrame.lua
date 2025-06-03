a81d03f74ec68f325fa001a3db16ace8885c5cd1
Zylenxx <99104881+Zylenxx@users.noreply.github.com>
2025-06-04 00:30:16 +0200

    Prettify tables

--- PrintFrame.lua
+++ PrintFrame.lua
@@ -1,3 +1,4 @@
+
 function PrintFrame(IN,depth,maxdepth)
 	local Contents = vgui.Create("DFrame")
 	Contents:Center()
@@ -10,7 +11,20 @@ function PrintFrame(IN,depth,maxdepth)
 	local function DoNodePopulate(From,IN,depth,maxdepth)
 		for k,v in pairs(IN) do
 			if Lenmax > 0 then
-				local Bulletin = From:AddNode(string.rep(" ",depth) .. k  ..":" .. tostring(v))	
+				local Bulletin = From:AddNode(k  ..":" .. tostring(v))
+				if istable(v) then Bulletin:SetIcon("icon16/application_view_list.png") end
+				if type(v)=="string" then Bulletin:SetIcon("icon16/font.png") end
+				if IsColor(v) then Bulletin:SetIcon("icon16/color_wheel.png") end
+				if type(v)=="function" then Bulletin:SetIcon("icon16/cog_go.png") end
+				if type(v)=="number" then Bulletin:SetIcon("icon16/calendar_view_day.png") end
+				if type(v)=="userdata" then Bulletin:SetIcon("icon16/database_gear.png") end
+				if type(v)=="boolean" then Bulletin:SetIcon("icon16/contrast_high.png") end
+				if type(v)=="nil" then Bulletin:SetIcon("icon16/cross.png") end -- should not happen
+				
+				function Bulletin:OnNodeSelected(panel)
+					
+					
+				end
 				if type(v) == "table" then
 					 depth=depth+1
 					 Lenmax=Lenmax-1
